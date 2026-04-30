f1a4f753b3150029bd50eb6288da0bcfe9956f6b
pranjali <pranjalinirmal92@gmail.com>
2026-04-30 19:48:50 +0530

    Introduced LoginAPI Data Driven test for POC of open CSV Library!
    Introduced the dataprovider util to pass the test data to loginapitest
    and the tests will be executed parallel!

--- src/test/java/com/api/utils/CSVReaderUtil.java
+++ src/test/java/com/api/utils/CSVReaderUtil.java
@@ -2,6 +2,7 @@ package com.api.utils;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Iterator;
 import java.util.List;
 
 import com.dataproviders.api.bean.UserBean;
@@ -11,7 +12,7 @@ import com.opencsv.bean.CsvToBeanBuilder;
 
 public class CSVReaderUtil {
 
-	public static void loadCSV(String pathOfCSVFile) {
+	public static Iterator<UserBean> loadCSV(String pathOfCSVFile) {
 
 		InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream(pathOfCSVFile);
 		InputStreamReader isr = new InputStreamReader(is);
@@ -24,7 +25,7 @@ public class CSVReaderUtil {
 				.build();
 
 		List<UserBean> userList = csvToBean.parse();
-		System.out.println(userList);
+		return userList.iterator();
 
 	}
 
